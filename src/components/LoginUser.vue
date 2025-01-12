3f437ef01b99c5faf03bfcc06ddcc9a22d7faef7
Carlos Alexander Duarte Hernadez <64160994+CarlosDuarte1998@users.noreply.github.com>
2025-01-11 19:00:01 -0600

    Commit initial

---
+++ src/components/LoginUser.vue
@@ -0,0 +1,65 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="h-screen flex justify-center items-center ">
+    <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-lg text-center">
+      <h1 class="text-2xl font-bold sm:text-3xl">Compra lo quieras <br>
+        <span class="text-5xl uppercase">¡nosotros te lo llevamos!</span></h1>
+
+      <p class="mt-4 text-gray-500">
+        Para calcular el costo de tu envío, ingresa una dirección de correo electrónico para dar el siguiente paso.
+      </p>
+    </div>
+
+    <form action="#" class="mx-auto mb-0 mt-8 max-w-md space-y-4">
+      <div>
+        <label for="email" class="sr-only">Correo Electrónico</label>
+
+        <div class="relative">
+          <input
+            type="email"
+            class="w-full rounded-lg border-gray-400 p-4 pe-12 text-lg font-bold shadow-lg"
+            placeholder="Enter email"
+          />
+
+          <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="size-4 text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+              />
+            </svg>
+          </span>
+        </div>
+      </div>
+
+
+
+      <div class="flex items-center justify-center ">
+
+        <button
+          type="submit"
+          class="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm mt-5 font-medium text-white"
+          >
+          Siguiente paso
+        </button>
+      </div>
+    </form>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LoginUser from '@/components/LoginUser.vue';
+
+
+</script>
+
+<template>
+  <main>
+    <LoginUser />
+  </main>
+</template>
